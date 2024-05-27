7f9ca45452ef08c2dbeb8368d23ec53eb90137f8
michaelwijanto <michael.wijanto@gmail.com>
2024-05-27 14:58:56 +0700

    init dnd for board

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,14 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import React from "react";
 
 function Board() {
-  return <div>Board</div>;
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>{/* render all columns */}</div>}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Board;
