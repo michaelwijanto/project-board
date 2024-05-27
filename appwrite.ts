df416711a4b72a9204bfd04cba8ca8d35f63990a
michaelwijanto <michael.wijanto@gmail.com>
2024-05-27 15:15:07 +0700

    init appwrite

---
+++ appwrite.ts
@@ -0,0 +1,13 @@
+import { Client, Account, ID, Databases, Storage } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.PUBLIC_APPWIRTE_PROJECT_ID!);
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, databases, storage, ID };
